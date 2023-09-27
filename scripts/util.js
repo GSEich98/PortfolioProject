9f1724a82d966312ccb49659191eb8e3fcf41db8
Graham Eichsteadt <GrahamE@ywcanwil.org>
2023-09-27 16:10:40 -0500

    Updated UI & split the website into multiple pages to improve performance. There are still ongoing stuttering issues

--- scripts/util.js
+++ scripts/util.js
@@ -6,3 +6,8 @@ function submitHandler() {
 function dismiss() {
     document.getElementById('thank-you').style.display = "none";
 }
+
+function isInViewport(element) {
+    // Check if any part of the element is in the viewport
+    return element.getBoundingClientRect().top <= innerHeight;
+}
